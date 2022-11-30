7c328bb02b6f193c59ba543314935611f58e7541
santhosh420 <santhoshsanjaiyan699@gmail.com>
2022-11-30 11:59:13 +0530

    changes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ a.onload = function(){
     var data = a.response
     var result = JSON.parse(data);
     console.log("Name"+" : "+ result[150].name.common," Region"+" : "+ result[150].region," Area"+" : " +result[150].area," Subregion asnbf"+" : " +result[150].subregion);
-      
+
     for(var i=0; i<result.length;i++){
         console.log("Name"+" : "+result[i].name.common," Region"+" : "+result[i].region," Subregion"+" : "+result[i].subregion);
 }
